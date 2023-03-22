092f90c6333db3abc135308cb757a9609b7bf74a
thev3ritas <81315628+mikhailcodes@users.noreply.github.com>
2023-03-22 14:53:55 -0500

    Update gpt.ts

--- src/modules/gpt.ts
+++ src/modules/gpt.ts
@@ -14,7 +14,7 @@ const summerizeArticle = async (content: any, complexity = 2) => {
     // const how_complex = complexity >= 3 ? 'university' : 'grade 5'
     const bullet_number = complexity >= 3 ? complexity * 2 : complexity;
 
-    const propmpt = `You are a journalist. Summerize and provide ${bullet_number} bullet points and 1-2 sentences for each bullet point of the article content below. \n\n The article content is here: ${content}.`
+    const propmpt = `You are a journalist. Summerize and provide ${bullet_number} bullet points and 1-2 sentences for each bullet point, using "-" to seperate each bullet of the article content below. \n\n The article content is here: ${content}.`
     const res = await api.sendMessage(propmpt)
 
     return {

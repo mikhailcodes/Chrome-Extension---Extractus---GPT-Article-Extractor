3321649588e527a3f220758ff1fae1b52cc9bd63
thev3ritas <81315628+mikhailcodes@users.noreply.github.com>
2023-04-10 10:21:11 -0500

    Update gpt.ts
    Fixed prompt and made adjustments to returned values

--- src/modules/gpt.ts
+++ src/modules/gpt.ts
@@ -4,55 +4,28 @@ const api = new ChatGPTAPI({
   apiKey: process.env.OPENAI_API_KEY || "",
   completionParams: {
     model: "gpt-3.5-turbo",
-    temperature: 0.5,
   },
 });
 
 const summerizeArticle = async (content: any, complexity = 2) => {
   console.time("API timer:");
-  // const how_complex = complexity >= 3 ? 'university' : 'grade 5'
-  //const bullet_number = complexity >= 3 ? complexity * 2 : complexity;
 
-  let prompt: string;
+  const bulletCount = complexity > 0 ? complexity * 2 : 2; // if complexity is less than 0, default to 2 bullet points. If not multiply by 2.   
 
-  if (complexity === 0) {
-    prompt = "0";
-  } else if (complexity === 1) {
-    prompt = `You are a journalist. Summerize the article in 2 bullet points and 1-2 sentences for each bullet point, ONLY return a JSON with the data so that the bullet points are all in an array. \n\n The article content is here: ${content}.`;
-  } else if (complexity === 2) {
-    prompt = `You are a journalist. Summerize the article in 4 bullet points and 1-2 sentences for each bullet point, ONLY return a JSON with the data so that the bullet points are all in an array. \n\n The article content is here: ${content}.`;
-  } else if (complexity === 3) {
-    prompt = `You are a journalist. Summerize the article in 6 bullet points and 1-2 sentences for each bullet point, ONLY return a JSON with the data so that the bullet points are all in an array. \n\n The article content is here: ${content}.`;
-  } else if (complexity === 4) {
-    prompt = `You are a journalist. Summerize the article in 8 bullet points and 1-2 sentences for each bullet point, ONLY return a JSON with the data so that the bullet points are all in an array. \n\n The article content is here: ${content}.`;
-  } else if (complexity === 5) {
-    prompt = `You are a journalist. Summerize the article in 10 bullet points and 1-2 sentences for each bullet point, ONLY return a JSON with the data so that the bullet points are all in an array. \n\n The article content is here: ${content}.`;
-  } else {
-    prompt = `You are a journalist. Summerize the article in 10 bullet points and 1-2 sentences for each bullet point, ONLY return a JSON with the data so that the bullet points are all in an array. \n\n The article content is here: ${content}.`;
-  }
-  console.log(prompt);
-  //const propmpt = `You are a journalist. Summerize and provide ${bullet_number} bullet points and 1-2 sentences for each bullet point, ONLY return a JSON with the data so that the bullet points are all in an array. \n\n The article content is here: ${content}.`
+  const prompt = `You are a journalist with a proficient level of English comprehension. Using markdown language, generate ${bulletCount} bulleted summary of the article below. \n\n The article content is here: ${content}.`;
+
   const res = await api.sendMessage(prompt);
-
-  //console.log(res)
-
-  const data = await (function () {
-    try {
-      return JSON.parse(res.text);
-    } catch (e) {
-      return res.text;
-    }
-  })();
+  const responseText = res.text.split('\n').filter(item => item.trim() !== '').map(item => item.replace(/^\s*-\s*/, ''));
 
   console.timeEnd("API timer:");
   return {
     id: res.id,
+    role: res.role,
     parentMessageId: res.parentMessageId,
-    message: data,
-    data_type: data.bullet_points ? "json" : "text",
+    message: responseText // returns in an array of each bullet point.
   };
 };
 
-const expoundArticle = async (url: string, parentMessageId: string) => {};
+const expoundArticle = async (url: string, parentMessageId: string) => { };
 
 export { summerizeArticle, expoundArticle };

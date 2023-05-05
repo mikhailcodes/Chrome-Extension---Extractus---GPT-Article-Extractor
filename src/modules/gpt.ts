44dbfa6ca98552ae3967716de06f6ce856b20085
Flubber129 <manharbhojwani93@gmail.com>
2023-05-05 16:10:20 +0100

    Update gpt.ts

--- src/modules/gpt.ts
+++ src/modules/gpt.ts
@@ -3,7 +3,7 @@ import { ChatGPTAPI } from "chatgpt";
 const api = new ChatGPTAPI({
   apiKey: process.env.OPENAI_API_KEY || "",
   completionParams: {
-    model: "gpt-4",
+    model: "gpt-3.5-turbo",
   },
 });
 
@@ -11,7 +11,7 @@ const summerizeArticle = async (content: any, complexity = 2) => {
   console.time("API timer:");
 
   const bulletCount = complexity > 0 ? complexity * 2 : 2; // if complexity is less than 0, default to 2 bullet points. If not multiply by 2.   
-  console.log( content );
+
   const prompt = `You are a journalist with a proficient level of English comprehension. Using markdown language, generate ${bulletCount} bulleted summary of the article below. \n\n The article content is here: ${content}.`;
 
   const res = await api.sendMessage(prompt);
